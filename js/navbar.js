948adfd201d00d41be18b8b02412270d565dc669
Wilfried Wilde <huilefrite@hotmail.fr>
2025-12-18 20:59:39 +0200

    icon menu set

--- js/navbar.js
+++ js/navbar.js
@@ -7,7 +7,7 @@ const navbarButton = selectedSection.querySelector('p');
 
 const dots = ` 
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" class="home-icon-dots">
-        <path d="M240-400q-33 0-56.5-23.5T160-480q0-33 23.5-56.5T240-560q33 0 56.5 23.5T320-480q0 33-23.5 56.5T240-400Zm240 0q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm240 0q-33 0-56.5-23.5T640-480q0-33 23.5-56.5T720-560q33 0 56.5 23.5T800-480q0 33-23.5 56.5T720-400Z" />
+        <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
     </svg>
 `;
 const colorNames = ['white', 'white-transparent', 'black', 'gray'];
